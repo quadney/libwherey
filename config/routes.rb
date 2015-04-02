1d9d915e5539aa6d9a97d85feea21e37092c9852
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2015-04-02 13:54:14 -0400

    removed user updating location in region, moved to zone

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
       end
       resources :regions, only: [:show, :edit, :update, :destroy] do
         resources :zones, only: [:index, :new, :create]
-        member do
-          get 'user_entered_region' #this is probably REALLY BAD PRACTICE using get...
-          get 'user_exited_region'
-        end
       end
       resources :zones, only: [:show, :edit, :update, :destroy] do
         member do
