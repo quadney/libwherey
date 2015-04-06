26d2204818187c2afd2c096ebfb3040c76c84fa1
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2015-04-06 19:25:41 -0400

    edited routes - no longer need to embed zones in regions because overridden with as_json

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,16 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       # Our resources will go here.
+      # /universities/{:id}/regions
       resources :universities do 
         resources :regions, only: [:index, :new, :create]
       end
-      resources :regions, only: [:show, :edit, :update, :destroy] do
-        resources :zones, only: [:index, :new, :create]
-        member do
-          get 'user_entered_region' #this is probably REALLY BAD PRACTICE using get...
-          get 'user_exited_region'
-        end
-      end
+      # /regions/{:id}
+      resources :regions, only: [:show, :edit, :update, :destroy]
+
+      # /zones/{:id}
       resources :zones, only: [:show, :edit, :update, :destroy] do
+        # /zones/{:id}/user_entered_zone
         member do
           get 'user_entered_zone'
           get 'user_exited_zone'
